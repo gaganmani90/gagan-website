66b866ec5daa8a68e882e73530ff0a8db818567e
Gagan Mani <gaganmani90@gmail.com>
2022-02-17 21:31:09 -0800

    Nav bar

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navbar,Nav,Container,NavbarBrand } from 'react-bootstrap'
+
+export const NavigationBar = () => {
+    return(
+        <Navbar collapseOnSelect fixed='top' expand='sm' bg='dark' variant='dark' style={{backgroundColor: "#071740", position: "sticky"}}>
+            <Container>
+                <NavbarBrand href='/'>Gagan Mani</NavbarBrand>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                <Navbar.Collapse id='responsive-navbar-nav'>
+                    <Nav>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                        <Nav.Link href='/Projects'>Projects</Nav.Link>
+                        <Nav.Link href='/Hobbies'>Hobbies</Nav.Link>
+                        <Nav.Link href='/Mentorship'>Mentorship</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
