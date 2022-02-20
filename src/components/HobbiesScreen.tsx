bb461e43127dc31574c3b121f56633e5a1683d66
Gagan Mani <gaganmani90@gmail.com>
2022-02-20 09:10:25 -0800

    add more YT videos

--- src/components/HobbiesScreen.tsx
+++ src/components/HobbiesScreen.tsx
@@ -23,6 +23,20 @@ export const HobbiesScreen = () => {
                             allowFullScreen></iframe>
                 </Col>
             </Row>
+            <Row>
+                <Col>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/AmQJJAqodvA"
+                            title="YouTube video player" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen></iframe>
+                </Col>
+                <Col>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/t3wABl73c5U"
+                            title="YouTube video player" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen></iframe>
+                </Col>
+            </Row>
         </Container>
     )
 }
